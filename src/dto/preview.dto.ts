7784d254a57d822e81fd608843f91f5c82874aca
yuvraj <yuvrajsab@protonmail.com>
2023-02-21 11:42:24 +0530

    remove unnecessary checks from dto

--- src/dto/preview.dto.ts
+++ src/dto/preview.dto.ts
@@ -2,17 +2,13 @@ import { IsNotEmpty, IsString } from 'class-validator';
 
 export class PreviewDto {
   @IsNotEmpty()
-  @IsString()
   url!: string;
 
   @IsNotEmpty()
-  @IsString()
   filename!: string;
 
-  @IsString()
   type?: string;
 
   @IsNotEmpty()
-  @IsString()
   configId!: string;
 }
